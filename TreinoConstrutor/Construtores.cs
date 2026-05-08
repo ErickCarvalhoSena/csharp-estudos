f00fc1ab03ed9da75a220bbdaa65c2b40c188e1f
Erick Sena <erick087sena@gmail.com>
2026-05-08 07:10:17 -0300

    Arquivos de estudo csharp

---
+++ TreinoConstrutor/Construtores.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreinoConstrutor
+{
+    internal class Construtores
+    {
+
+        public class Pessoa
+        {
+            public string Nome { get; set; }
+            
+            public Pessoa(string nome)
+            {
+                Nome = nome;
+            }
+        }
+
+        public void exibir()
+        {
+            Produto produto = new Produto();
+            Console.WriteLine($"Produto: {produto.Nome}, Preço: {produto.Preco}");
+        }
+
+         public class Produto
+        { 
+           public string Nome { get; set; }
+           public double Preco { get; set; }
+
+            public Produto()
+            {
+                Nome = "Sem nome";
+                Preco = 0.0;
+            }
+        }
+    }
+}
---
+++ TreinoConstrutor/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace TreinoConstrutor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Construtores construtores = new Construtores();
+            construtores.exibir();
+        }
+    }
+}
